aeeb00059551fd2ec2523d80756010396a8c4e05
hemmerling <hemmerling@gmx.net>
2017-04-10 13:32:54 +0200

    Initial Release

---
+++ Grundlagen/src/variablen/Datentypen.java
@@ -0,0 +1,22 @@
+package variablen;
+
+public class Datentypen {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test");
+		int x;
+		x = 3;
+		System.out.println(x);
+		// Liste der Typen
+		boolean bo;
+		char ch;
+		
+		byte b;
+		short s;
+		long y;
+		float f;
+		double d;
+	}
+
+}
