3dca224e55fc56641aadbfbbac6694ef02f453ad
hemmerling <hemmerling@gmx.net>
2017-04-20 13:16:07 +0200

    update

---
+++ Projekte/Grundlagen/Klassen/sonnensystem/v1/Sonnensystem.java
@@ -0,0 +1,18 @@
+package sonnensystem.v1;
+
+public class Sonnensystem {
+	
+	static void printPlanet(int x, int y, int z ){
+		System.out.println(x+ "," + y + "," + z);
+	}
+
+	public static void main(String[] args) {
+		int xErde = 20;
+		int yErde = 30;
+		int zErde = 30;
+		printPlanet(xErde, yErde, zErde );
+		// TODO Auto-generated method stub
+
+	}
+
+}
