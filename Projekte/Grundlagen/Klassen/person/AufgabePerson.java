0189ad996d8ef34bbaed906bdc3be2d8cb029a28
hemmerling <hemmerling@gmx.net>
2017-04-24 12:23:53 +0200

    Update

---
+++ Projekte/Grundlagen/Klassen/person/AufgabePerson.java
@@ -0,0 +1,19 @@
+package person;
+
+public class AufgabePerson {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Person person1 = new Person();
+		person1.setVorname( "Hans" );
+	    person1.setNachname( "Meyer" );
+	    person1.setPostleitzahl("12345");
+	    person1.setGeburtsjahr( 1950 );
+	   
+	    // Hans Meyer, Geburtsjahr: 1950, Plz: 12345
+	    System.out.println ( person1.getVorname() + " " + person1.getNachname() + 
+	    					 ", Geburtsjahr:" + person1.getGeburtsjahr() + 
+	    					 ", Plz: " + person1.getPostleitzahl());
+	}
+
+}
---
+++ Projekte/Grundlagen/Klassen/person/Person.java
@@ -0,0 +1,55 @@
+package person;
+
+public class Person {
+
+	private String vorname, nachname, postleitzahl;
+	private int geburtsjahr;
+	
+	public Person(String vorname, String nachname, String postleitzahl, int geburtsjahr){
+		this.vorname = vorname;
+		this.nachname = nachname;
+		this.postleitzahl = postleitzahl;
+		this.geburtsjahr = geburtsjahr;		
+	}
+	
+	public Person(){
+		}
+	
+    public String getVorname(){
+    	return this.vorname;
+    }
+    
+    public void setVorname( String vorname ){
+		this.vorname = vorname;    	
+    }
+
+    public String getNachname(){
+    	return this.nachname;
+    }
+    
+    public void setNachname( String nachname ){
+		this.nachname = nachname;    	
+    }
+
+    public String getPostleitzahl(){
+    	return this.postleitzahl;
+    }
+    
+    public void setPostleitzahl( String postleitzahl ){
+		this.postleitzahl = postleitzahl;    	
+    }
+
+    public int getGeburtsjahr(){
+    	return this.geburtsjahr;
+    }
+    
+    public void setGeburtsjahr( int geburtsjahr ){
+		this.geburtsjahr = geburtsjahr;    	
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
