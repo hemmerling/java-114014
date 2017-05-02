9c744633bcad3333fde71694aae1e0f8c555c306
hemmerling <hemmerling@gmx.net>
2017-05-02 12:08:40 +0200

    Update

--- src/api/AufgabeScannerTest.java
+++ src/api/AufgabeScannerTest.java
@@ -14,9 +14,35 @@ public class AufgabeScannerTest {
 	}
 
 	@Test
-	public void test() {
-		String expected = "3";
-		//assertEquals("Scanner - Addition", expected, aufgabeScanner.);
+	public void testOperation() {
+		char operator = '+';
+		double operand1	 = 2.0;
+		double operand2 = 3.0;
+		double expected = 5.0;
+		assertEquals("Scanner - Addition", String.valueOf(expected), 
+ 				     String.valueOf(aufgabeScanner.operation(operator, operand1, operand2)));
+		//fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInputCommandline() {
+		String operator = "+";
+		String operand1	 = "2.0";
+		String operand2 = "3.0";
+		String[] expected = { operand1, operator, operand2 };
+		assertArrayEquals(expected, aufgabeScanner.inputCommandline(expected));
+		//fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testProcessOperation1() {
+		String operator = "+";
+		String operand1	 = "2.0";
+		String operand2 = "3.0";
+		String[] input = { operand1, operator, operand2 };
+		String expected = "5.0";
+		assertEquals("Scanner - Addition", expected, 
+ 				     String.valueOf(aufgabeScanner.processOperation1(input)));
 		//fail("Not yet implemented");
 	}
 
