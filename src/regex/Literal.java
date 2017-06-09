58909e2d8716f428b322dcaff4318b301f2e5eed
hemmerling <hemmerling@gmx.net>
2017-06-09 05:16:12 +0200

    Update

---
+++ src/regex/Literal.java
@@ -0,0 +1,33 @@
+package regex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
+public class Literal {
+
+	boolean isDecimalLiteral(String text) {
+		boolean result = false;
+
+		String lit1 ="^\\+*\\-*[0-9][0-9]\\z";
+				
+		Pattern p = Pattern.compile(lit1);		
+		Matcher m = p.matcher(text);
+		result = m.find(); 
+
+		return result;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Literal literal = new Literal();
+
+		System.out.println(literal.isDecimalLiteral("10"));    // - Sollwert: true
+		System.out.println(literal.isDecimalLiteral("-10"));   // - Sollwert: true
+		System.out.println(literal.isDecimalLiteral("+10"));   // - Sollwert: true
+		System.out.println(literal.isDecimalLiteral("abc10")); // - Sollwert: false
+		System.out.println(literal.isDecimalLiteral("10abc")); // - Sollwert: false
+
+	}
+
+}
---
+++ src/regex/StringTokens.java
@@ -0,0 +1,60 @@
+package regex;
+
+public class StringTokens {
+
+	public static void main(String[] args) {
+		
+		String str = "Mo Di Mi";
+		
+		String regex = " "; //trenner
+		
+		String[] tokens = str.split(regex);
+		for (String subStr : tokens) {
+			System.out.println(">" + subStr + "<");
+		}
+
+		System.out.println("--------------------------------");
+		/*
+		 * Exam: Sauber Mo Di und Mi kriegen
+		 */
+		
+		str = "Mo. Di. Mi";
+		//regex = "\.\s";		//Achtung! Compilerfehler!
+		regex = "\\.\\s";
+		
+		tokens = str.split(regex);
+		for (String subStr : tokens) {
+			System.out.println(">" + subStr + "<");
+		}
+		
+		System.out.println("--------------------------------");
+		/*
+		 * Exam: Sauber Mo Di und Mi kriegen
+		 */
+		
+		str = "Mo. Di. Mi.";
+		regex = "\\.\\s";
+		
+		tokens = str.split(regex);
+		for (String subStr : tokens) {
+			System.out.println(">" + subStr + "<");
+		}
+
+		
+		System.out.println("-Hausaufgabe--------------------");
+		/*
+		 * Exam: Sauber Mo Di und Mi kriegen
+		 */
+		
+		str = "Mo. Di. Mi.";
+		regex = "\\.\\s*";
+		
+		tokens = str.split(regex);
+		for (String subStr : tokens) {
+			System.out.println(">" + subStr + "<");
+		}
+
+	}
+
+}
+
