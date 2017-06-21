872f1867921308d2e41657be66e8894cdceac2a2
hemmerling <hemmerling@gmx.net>
2017-06-21 13:04:10 +0200

    Update

---
+++ src/pruefungsvorbereitung/Vce2014B04.java
@@ -0,0 +1,19 @@
+package pruefungsvorbereitung;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class Vce2014B04 {
+	
+	public static void main(String[] args) {
+		Date date = new Date();
+		int style = DateFormat.MEDIUM;
+		DateFormat df;
+		df = DateFormat.getDateInstance(style, Locale.ITALY);
+		//df.setLocale(Locale.ITALY);
+		String s = df.format(date);
+		System.out.println(s);
+	}
+
+}
